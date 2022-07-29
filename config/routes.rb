29db962cccb0bad9855a682456dad12e5a15235b
GaneshDeokar <86193892+GaneshDeokar@users.noreply.github.com>
2022-07-29 10:48:27 +0530

    after routes chapter commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  resources :articles,only: [:show]
+  # get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
