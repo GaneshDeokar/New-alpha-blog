d9313c6141d1021598d695694cfe5898c3623c4e
GaneshDeokar <86193892+GaneshDeokar@users.noreply.github.com>
2022-08-10 15:54:46 +0530

    add user create action

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :articles,only: [:show,:index,:new,:create,:edit,:update,:destroy]
   resources :articles
   get 'signup', to:'users#new'
-  post 'users', to:'users#create'
+  resources:users, except:[:new]
 end
